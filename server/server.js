46f36a3fb5465cf1b7a8c6776242162617629fb6
Raymond <imleeraymond@gmail.com>
2018-09-03 10:14:20 -0700

    add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -36,12 +36,9 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
 	var id = req.params.id;
 
-
 	if(!ObjectID.isValid(id)){
 		return res.status(404).send();
 	}
-
-
 
 	Todo.findById(id).then((todo) => {
 		if(!todo){
@@ -55,6 +52,32 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
+
+app.delete('/todos/:id', (req, res) => {
+	//get the id
+	var id = req.params.id;
+
+	//validate the id -> not valid? return 404
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+
+		res.status(200).send(todo);
+	}).catch((e) => {
+		res.status(400).send();
+	});
+	//remove todo by id
+		//success
+			//if no doc, send 404
+			//if doc, send doc back with 200
+		//error
+			//400 with empty body
+});
 
 app.listen(port, () => {
 	console.log(`Started up at port ${port}`);
